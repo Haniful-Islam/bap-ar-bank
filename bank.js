5ec5b4763f31e57e2dbcef85326507a7b81922e8
Haniful Islam <haniful360@gmail.com>
2022-10-28 20:21:52 +0600

    bap aer bank added

---
+++ bank.js
@@ -0,0 +1,15 @@
+document.getElementById('submit-button').addEventListener('click', function(){
+    // get userEmail
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    console.log(userEmail)
+
+    // get userPassword
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    console.log(userPassword)
+     
+    if(userEmail == 'abc@gmail.com' && userPassword == 1234){
+        window.location.href = 'banking.html';
+    }
+})
